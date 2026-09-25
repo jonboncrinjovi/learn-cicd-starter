12350c23c45090280a6dda0ddbd23c4fd2727a8e
jonboncrinjovi <georgvangeorg@gmail.com>
2026-09-25 16:19:29 +0300

    gosecs

--- main.go
+++ main.go
@@ -89,9 +89,9 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
+		Addr:              ":" + port,
 		ReadHeaderTimeout: 5 * time.Second,
-		Handler: router,
+		Handler:           router,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
