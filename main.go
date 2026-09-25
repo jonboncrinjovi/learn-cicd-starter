2acb4a47b58a385e717335236fb61ce0b2be9c54
jonboncrinjovi <georgvangeorg@gmail.com>
2026-09-25 16:32:24 +0300

    gosecs

--- main.go
+++ main.go
@@ -95,6 +95,6 @@ func main() {
 		Handler:           router,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %s", strconv.Quote(port))
 	log.Fatal(srv.ListenAndServe())
 }
