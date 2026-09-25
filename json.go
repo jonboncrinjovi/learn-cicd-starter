aac6ba0cb2811e14dc543bbcc9656db0857844a7
jonboncrinjovi <georgvangeorg@gmail.com>
2026-09-25 16:14:27 +0300

    gosec fiz

--- json.go
+++ json.go
@@ -31,6 +31,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	}
 	w.WriteHeader(code)
 	if _, err := file.Write([]byte("data")); err != nil {
-		log.Printf("Error marshalling JSON: %s", err
+		log.Printf("Error marshalling JSON: %s", err)
 	}
 }
